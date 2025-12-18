a58d8d2d92ee35ce7791715a552987a9045ed2d3
cahrithikn-crypto <cahrithikn@gmail.com>
2025-12-19 00:24:12 +0530

    Update orders.js

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -1,17 +1,17 @@
-import { connectDB } from "../../lib/db";
-import Order from "../../models/Order";
+import { connectDB } from "../../../lib/db";
+import Product from "../../../models/Product";
 
 export default async function handler(req, res) {
   await connectDB();
 
-  if (req.method === "POST") {
-    const order = await Order.create(req.body);
-    return res.status(201).json(order);
+  if (req.method === "GET") {
+    const products = await Product.find();
+    return res.status(200).json(products);
   }
 
-  if (req.method === "GET") {
-    const orders = await Order.find().sort({ createdAt: -1 });
-    return res.status(200).json(orders);
+  if (req.method === "POST") {
+    const product = await Product.create(req.body);
+    return res.status(201).json(product);
   }
 
   return res.status(405).json({ message: "Method not allowed" });
