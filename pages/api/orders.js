39b0de7d850c20a3fc78568ba809e02fa73473d0
cahrithikn-crypto <cahrithikn@gmail.com>
2025-12-19 00:59:03 +0530

    Update orders.js

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -1,5 +1,5 @@
-import { connectDB } from "../../../lib/db";
-import Order from "../../../models/Order";
+import { connectDB } from "../lib/db";
+import Order from "../models/Order";
 
 export default async function handler(req, res) {
   await connectDB();
