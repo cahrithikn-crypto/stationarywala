3e246bea6dbaae0b299e57d5c0a0bc4f7d9e2bd9
cahrithikn-crypto <cahrithikn@gmail.com>
2025-12-18 23:43:01 +0530

    Add files via upload

---
+++ pages/success.js
@@ -0,0 +1,8 @@
+export default function Success() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>✅ Payment Successful</h1>
+      <p>Thank you for shopping with Stationarywala!</p>
+    </div>
+  );
+}
