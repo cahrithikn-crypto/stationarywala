321135c8b402d3dc196f76ff2ffb25062f6f93e0
cahrithikn-crypto <cahrithikn@gmail.com>
2025-12-20 13:54:36 +0530

    Delete pages directory

--- pages/success.js
+++
@@ -1,8 +0,0 @@
-export default function Success() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>✅ Payment Successful</h1>
-      <p>Thank you for shopping with Stationarywala!</p>
-    </div>
-  );
-}
