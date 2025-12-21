e711090f89b61f565e7d45d3ef2d74094ad221b2
cahrithikn-crypto <cahrithikn@gmail.com>
2025-12-21 15:39:16 +0530

    Update admin.js

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,4 +1,4 @@
-import "./globals.css";
+import { useState, useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
@@ -162,4 +162,5 @@ export default function Admin() {
     </div>
   );
 }
+
 
