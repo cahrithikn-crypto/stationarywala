079323d65491406938e195cdbd35d0a3c9eab333
cahrithikn-crypto <cahrithikn@gmail.com>
2025-12-21 17:00:41 +0530

    Update Product.js

--- src/models/Product.js
+++ src/models/Product.js
@@ -2,22 +2,10 @@ import mongoose from "mongoose";
 
 const ProductSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true
-    },
-    price: {
-      type: Number,
-      required: true
-    },
-    stock: {
-      type: Number,
-      required: true
-    },
-    image: {
-      type: String,
-      default: ""
-    }
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    stock: { type: Number, required: true },
+    image: { type: String, required: true }, // IMAGE URL
   },
   { timestamps: true }
 );
