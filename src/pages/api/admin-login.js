5e6d7693c8dd090c117d6c8a68ad4ac94ab790ce
cahrithikn-crypto <cahrithikn@gmail.com>
2025-12-21 16:32:31 +0530

    Update admin-login.js

--- src/pages/api/admin-login.js
+++ src/pages/api/admin-login.js
@@ -1,13 +1,11 @@
 export default function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).end();
-  }
+  if (req.method !== "POST") return res.status(405).end();
 
   const { password } = req.body;
 
   if (password === process.env.ADMIN_PASSWORD) {
-    return res.status(200).json({ success: true });
+    return res.json({ success: true });
   }
 
-  return res.status(401).json({ success: false });
+  res.status(401).json({ success: false });
 }
